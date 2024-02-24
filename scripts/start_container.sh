9a20614aa9d1e1a45257b9952524281849f68c22
divyachintha <divyachintha5@gmail.com>
2024-02-24 23:23:43 +0530

    dummy commit3

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,3 +6,4 @@ docker pull divyachintha/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 divyachintha/simple-python-flask-app
+echo "Deployment is completed"
